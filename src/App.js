4ff17597fc8606db621142c464ba2b898189a86d
Albert Tarkaa <tarkaa.albert@gmail.com>
2021-09-25 10:40:40 +0100

    header setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import './App.css';
+import Header from './components/header';
 import Homepage from './pages/homepage';
 import ShopPage from './pages/shop';
 
 function App() {
   return (
-    <div>
-      <Route exact path='/' component={Homepage} />
-      <Route path='/shop' component={ShopPage} />
-    </div>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </>
   );
 }
 
