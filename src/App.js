688522d206e82f18321bc8749a515abc544819f2
Albert Tarkaa <tarkaa.albert@gmail.com>
2021-10-06 11:58:23 +0100

    updated signin to redirect to homepage if signed in and user action types updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Route, Switch } from 'react-router';
+import { Redirect, Route, Switch } from 'react-router';
 import './App.css';
 import Header from './components/header';
 import { auth, createUserProfileDocument } from './firebase/firebase.util';
@@ -40,13 +40,17 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInUp} />
+          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) :( <SignInUp />)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = (dispatch) => ({ setCurrentUser: (user) => dispatch(setCurrentUser(user)) });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
