cd81b091a981469e62c4d0c9aaccd347b80bc7da
Albert Tarkaa <tarkaa.albert@gmail.com>
2021-10-10 15:27:50 +0100

    introduces selectors for cartitems and dropdown using the reslect package

--- src/components/cart-dropdown.js
+++ src/components/cart-dropdown.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectCartItems } from '../redux/cart/cartSelectors';
 import './cart-dropdown.styles.scss';
 import CartItem from './cart-item';
 import CustomButtom from './customButton';
@@ -17,7 +18,7 @@ const CartDropDown = ({ cartItems }) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 export default connect(mapStateToProps)(CartDropDown);
